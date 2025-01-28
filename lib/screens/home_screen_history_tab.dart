af812b0b37e0b3d40a457e0ded5e9d4a2d51f1cc
Thomas Duffin <thomaslukeduffin@hotmail.co.uk>
2025-01-28 23:23:55 +0000

    fix nav bar colors and add some basic history tab functionality for debugging

--- lib/screens/home_screen_history_tab.dart
+++ lib/screens/home_screen_history_tab.dart
@@ -1,12 +1,38 @@
-import 'package:flutter/widgets.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:serealappv2/models/providers/log_providers.dart';
+import 'package:serealappv2/models/types/daily_log.dart';
 
-class HomeScreenHistoryTab extends StatelessWidget {
+class HomeScreenHistoryTab extends ConsumerWidget {
   const HomeScreenHistoryTab({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return const Center(
-      child: Text('History coming soon!'),
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Center(
+      child: ref.watch(filteredLogsProvider(filter: null)).when(
+            data: (data) => ListView(
+              children: List.generate(
+                data.length,
+                (index) {
+                  final DailyLog log = data[index];
+                  return Card(
+                    child: Column(
+                      children: [
+                        Text(log.id),
+                        Text(log.date.toString()),
+                        Text(log.meals.toString()),
+                        Text(log.snacks.toString()),
+                        Text(log.todos.toString()),
+                        Text(log.note.toString()),
+                      ],
+                    ),
+                  );
+                },
+              ),
+            ),
+            error: (Object error, StackTrace stackTrace) => null,
+            loading: () => CircularProgressIndicator(),
+          ),
     );
   }
 }
