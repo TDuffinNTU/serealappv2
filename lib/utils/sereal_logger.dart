772d475f4ed97a0d60d23811f52a36fffec298ee
Thomas Duffin <thomaslukeduffin@hotmail.co.uk>
2025-09-28 20:55:16 +0100

    remove logger class in favour of mixin

--- lib/utils/sereal_logger.dart
+++ lib/utils/sereal_logger.dart
@@ -1,33 +1,5 @@
 import 'dart:developer';
 
-class SerealLogger {
-  static void info({
-    required String message,
-    required String name,
-  }) {
-    log(
-      message,
-      name: name,
-      time: DateTime.now(),
-    );
-  }
-
-  static void error({
-    required String message,
-    required String name,
-    required StackTrace stacktrace,
-    required Object error,
-  }) {
-    log(
-      message,
-      name: name,
-      stackTrace: stacktrace,
-      error: error,
-      time: DateTime.now(),
-    );
-  }
-}
-
 mixin SerealLoggerMixin {
   void logInfo(String message) {
     log(
